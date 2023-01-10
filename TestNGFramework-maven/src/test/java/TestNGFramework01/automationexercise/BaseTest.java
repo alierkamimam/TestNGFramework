e885de941746f1fdbb849549b99d378121e398d5
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-10 23:50:01 +0300

    added

---
+++ TestNGFramework-maven/src/test/java/TestNGFramework01/automationexercise/BaseTest.java
@@ -0,0 +1,7 @@
+package TestNGFramework01.automationexercise;
+
+import TestNGFramework01.utilities.Pages;
+
+public class BaseTest {
+    protected Pages pages = new Pages();
+}
---
+++ TestNGFramework-maven/src/test/java/TestNGFramework01/pages/BasePage.java
@@ -0,0 +1,28 @@
+package TestNGFramework01.pages;
+
+import TestNGFramework01.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class BasePage {
+    protected WebDriver driver= Driver.getDriver();//--->utilities.Driver
+    protected WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+
+    public BasePage() {
+        PageFactory.initElements(driver,this);
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
+    }
+
+    public void navigateTo(String component){
+        String locator= "//a[text() = '" + component + "']";
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
+
+    }
+}
