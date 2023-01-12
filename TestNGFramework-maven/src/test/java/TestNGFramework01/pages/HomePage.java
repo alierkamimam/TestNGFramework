7702f41338c399ae2cbf304bf0ee9064482e3d9c
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-12 14:20:24 +0300

    added

--- TestNGFramework-maven/src/test/java/TestNGFramework01/pages/HomePage.java
+++ TestNGFramework-maven/src/test/java/TestNGFramework01/pages/HomePage.java
@@ -1,9 +1,9 @@
 package TestNGFramework01.pages;
 
 import TestNGFramework01.utilities.BrowserUtils;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 
 
 public class HomePage extends BasePage {
@@ -14,6 +14,23 @@ public class HomePage extends BasePage {
     @FindBy(css = ".navbar-nav a[href='/products']")
     private WebElement productButton;
 
+    @FindBy(xpath = "//a[text()=' Cart']")
+    private WebElement cartButton;
+
+    @FindBy(xpath = "//a[text()=' Test Cases']")
+    private WebElement testCasesButton;
+
+    @FindBy(xpath = "//a[text()=' API Testing']")
+    private WebElement apiTestingButton;
+
+    @FindBy(xpath = "//li[10]//a[1]")
+    private WebElement loggedInASName;
+
+    @FindBy(css = "a[href='/delete_account']")
+    private WebElement clickDeleteAccount;
+
+    @FindBy(css = "h2[class='title text-center'] b")
+    private WebElement accountDeletedMessage;
 
 
     public void clickSignupLoginButton() {
@@ -25,5 +42,18 @@ public class HomePage extends BasePage {
         BrowserUtils.verifyElementDisplayed(productButton);
         productButton.click();
     }
+
+    public String loggedMessage() {
+        return loggedInASName.getText();
+    }
+
+    public void setClickDeleteAccount() {
+        clickDeleteAccount.click();
+    }
+
+    public String accountDeletedMessage() {
+        return accountDeletedMessage.getText();
+    }
+
 
 }
