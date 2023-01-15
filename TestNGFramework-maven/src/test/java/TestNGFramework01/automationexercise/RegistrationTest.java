eb0ce5f9e6d2c90169a86367cc36dc376fdecec9
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-15 16:21:36 +0300

    added

--- TestNGFramework-maven/src/test/java/TestNGFramework01/automationexercise/RegistrationTest.java
+++ TestNGFramework-maven/src/test/java/TestNGFramework01/automationexercise/RegistrationTest.java
@@ -32,17 +32,22 @@ public class RegistrationTest extends BaseTest {
         //Launch browser and Navigate to url 'http://automationexercise.com'
         //Click on 'Signup / Login' button
         pages.getHomePage().clickSignupLoginButton();
-        String newUserSignUpMessage = pages.getLoginPage().setNewUserSignUpMessage();
 
         //Verify 'New User Signup!' is visible
+        String newUserSignUpMessage = pages.getLoginPage().setNewUserSignUpMessage();
         softAssert.assertEquals(newUserSignUpMessage, "New User Signup!", "Test Case 1 - Verify 'New User Signup!' is visible");
-        //LoginPage
-        pages.getLoginPage().setSignupNameBox("erkam4123");
-        pages.getLoginPage().setSignupEmailAddressBox("erk1234@mail.com");
+
+        //Enter name and email address
+        pages.getLoginPage().setSignupNameBox("testCase01");
+        pages.getLoginPage().setSignupEmailAddressBox("testCase01@mail.com");
+
+        // Click 'Signup' button
         pages.getLoginPage().setSignupButton();
+
+        //Dismiss pop-ups by navigating back and forward page
         BrowserUtils.navigateBackAndForwardToDismissAds();
 
-        //SignUpPage
+        //Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
         pages.getSignupPage().setSelectButtonMen();
         pages.getSignupPage().setPasswordBox("12345");
         pages.getSignupPage().setDate("1", "Month", "1997");
@@ -57,21 +62,26 @@ public class RegistrationTest extends BaseTest {
         pages.getSignupPage().setZipCodeBox("0990");
         pages.getSignupPage().setMobileNumberBox("0555");
         BrowserUtils.wait(3);
+        //Click 'Create Account button'
         pages.getAccountCreatedPage().createAccountButton();
 
+        //Verify that 'ACCOUNT CREATED!' is visible
         String accountCreatedMessage = pages.getAccountCreatedPage().accountCreatedMessage();
         softAssert.assertEquals(accountCreatedMessage, "ACCOUNT CREATED!", "Test Case 1 - Verify 'Account Created!' is visible");
 
+        //Click 'Continue' button
         pages.getAccountCreatedPage().continueButton();
         BrowserUtils.navigateBackAndForwardToDismissAds();
 
+        //Verify that 'Logged in as username' is visible.
         String loggedMessage = pages.getHomePage().loggedMessage();
+        softAssert.assertEquals(loggedMessage, "Logged in as testCase01", "Test Case 1 - Verify 'Logged in as <name>' is visible");
 
-        softAssert.assertEquals(loggedMessage, "Logged in as erkam123", "Test Case 1 - Verify 'Logged in as <name>' is visible");
-
+        //Click 'Delete Account' button
         pages.getHomePage().setClickDeleteAccount();
         BrowserUtils.wait(3);
 
+        //Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
         String actualAccountDeletedMessage = pages.getAccountDeletePage().getAccountDeletedMessage();
         softAssert.assertEquals(actualAccountDeletedMessage, "ACCOUNT DELETED!", "Test Case 1 - Verify 'Account Deleted!' is visible");
         pages.getAccountDeletePage().getClickContinueButton();
