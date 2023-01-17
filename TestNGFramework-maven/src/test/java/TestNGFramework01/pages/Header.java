0facfd53d4bf0f7f56afa777cef9a657d132e3c3
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-17 12:04:43 +0300

    added

--- TestNGFramework-maven/src/test/java/TestNGFramework01/pages/Header.java
+++ TestNGFramework-maven/src/test/java/TestNGFramework01/pages/Header.java
@@ -7,8 +7,17 @@ public class Header extends BasePage {
     @FindBy(css = "a[href='/logout']")
     private WebElement logOutButton;
 
+    @FindBy(css = "a[href='/contact_us']")
+    private WebElement contactUs;
 
-    public void clickLogOutButton(){
+
+    public void clickLogOutButton() {
         logOutButton.click();
+    }
+
+    public void clickContactUs() {
+        contactUs.click();
+
+
     }
 }
